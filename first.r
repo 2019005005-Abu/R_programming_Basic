d8cf17d14dcd3b6e1dfba53524befe24165b30b8
Abu Al Shahriar Rifat <rifatrifatsharda2686@gmail.com>
2023-06-05 20:22:52 +0600

    R Programming Basic

---
+++ first.r
@@ -0,0 +1,45 @@
+print("Hello");
+print("I am doing R programming Language");
+##Data Types in R programming language
+##1.Atomic vector
+##2.List
+##3.Array
+##4.Matrics
+##5.Data Frame
+##6.Factors
+print("Welcome to P programming")
+a=8.9
+print(class(a));
+print(typeof(a));
+b=89
+print(class(b))
+print(typeof(b))
+print(is.integer(a));
+print(is.integer(b));
+print(is.integer(5));
+print(class(b));
+bL=5L
+print(class(bL))
+
+
+#Logical Data Types
+print(10>9);
+print(10==10);
+print(100<1)
+if(100>120){
+    print("100 is les than 120")
+}else{
+    print("100 is greater than 120")
+}
+
+if (100==100)print("True")
+
+char="Abu Al Shahriar Rifat";
+print(class(char));
+print(typeof(char));
+print(class(TRUE));
+print(class(3L));
+print(class(10.5));
+print(class(1+2i));
+print(class("12/4/2020"));
+
